a93662ff160af093478dc962ed055cfd10668dac
Samuel Dias <161338944+SamuelGFDias@users.noreply.github.com>
2024-10-11 16:22:25 -0300

    Método estático para injeção de todos os serviços do pacote

---
+++ SankhyaAPI.Client/Extensions/SankhyaApiExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SankhyaAPI.Client.Extensions;
+
+public static class SankhyaApiExtensions
+{
+    public static IServiceCollection AddSankhyaClient(this IServiceCollection services)
+    {
+        var typeInterface = typeof(ISankhyaApi);
+        var implementation = typeInterface.Assembly
+            .GetTypes()
+            .Where(t => !t.IsInterface
+                        && t != typeInterface
+                        && !t.IsGenericType
+                        && t.IsAssignableTo(typeInterface));
+
+        foreach (var type in implementation) services.AddScoped(type);
+        return services;
+    }
+}
--- SankhyaAPI.Client/Providers/SankhyaClientSettings.cs
+++ SankhyaAPI.Client/Providers/SankhyaClientSettings.cs
@@ -1,8 +1,37 @@
 ﻿namespace SankhyaAPI.Client.Providers;
 
-public class SankhyaClientSettings(string username, string password, string baseUrl)
+/// <summary>
+/// Classe de configuração para utilização dos serviços da API Sankhya.
+/// </summary>
+/// <remarks>
+/// Para utilizar os serviços da Sankhya, é necessário informar as credenciais de acesso (usuário e senha) e a URL base da API.
+/// Essas informações devem ser fornecidas no arquivo de configuração `appsettings.json`.
+/// </remarks>
+/// <example>
+/// Exemplo de uso:
+/// <code>
+/// var settings = new SankhyaClientSettings
+/// {
+///     Usuario = "usuario",
+///     Senha = "senha",
+///     BaseUrl = "https://api.sankhya.com.br"
+/// };
+/// </code>
+/// </example>
+public class SankhyaClientSettings
 {
-    public string Usuario => username;
-    public string Senha => password;
-    public string BaseUrl => baseUrl;
+    /// <summary>
+    /// O nome de usuário para acessar a API Sankhya.
+    /// </summary>
+    public required string Usuario { get; set; }
+
+    /// <summary>
+    /// A senha correspondente ao usuário para acessar a API Sankhya.
+    /// </summary>
+    public required string Senha { get; set; }
+
+    /// <summary>
+    /// A URL base da API Sankhya.
+    /// </summary>
+    public required string BaseUrl { get; set; }
 }
